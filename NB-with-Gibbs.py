36b8c59151af6e863e9b7a00e8d75d9d2dd572c2
Alex Kong <kqingchao@gmail.com>
2012-09-04 15:30:15 +0800

    add evaluation code: purity supported

--- NB-with-Gibbs.py
+++ NB-with-Gibbs.py
@@ -29,6 +29,7 @@ This means if you want find a word with a specified index , you have to iterate 
 and it becomes much simpler when you have word in string form, like "mini"
 """
 vocabulary = dict({"a":[0,0]}) # vocabulary for the whole dataset
+class_map = dict({"course":0, "faculty":1})
 
 # Add words in vocabulary
 def add_in_dict(line):
@@ -74,19 +75,26 @@ Note: Only load two categories: course and faculty.
 def load_data(datafile):
     docset = []
     docindexset = []
+    docnum = [0, 0]
     f = open(datafile, "r")
     for line in f:  # one document in each line
         line = line.replace("\t", " ")
         line = line.split(" "); # tokenize the document
         del line[-1] # delete the \n
-        print "Line[0]:" + line[0]
+        #print "Line[0]:" + line[0]
+        if line[0] == "project":
+            line[0] = "course"
+        if line[0] == "student":
+            line[0] = "faculty"
         if line[0] == "course" or line[0] == "faculty":
+            cindex = class_map[line[0]]
+            docnum[cindex] = docnum[cindex] + 1
             add_in_dict(line) # add words in vocabulary
             docindex = create_index(line) # create index for every word in every document
             docset.append(line)
             docindexset.append(docindex)
     #print docset
-    return (docset, docindexset)
+    return (docset, docindexset, tuple(docnum))
 
 """
 Count word for each class
@@ -154,17 +162,55 @@ def update_theta(docindexset, labels, theta, hyper_multi, word_count):
         for i in range(V):
             t[i] = word_count[c][i] + hyper_multi[i]
         theta[c] = Dirichlet(t)
+        
+"""
+Evaluate the clustering result
+@docnum number of docs in every class
+methods to be used: purity, NML, RI, F-value
+"""
+def evaluate_result(labels, docset, N, docnum):
+    print "Clustering evaluation result:"
+    for i in range(N):
+        doc = docset[i]
+        count0 = 0 # count number of "course" docs in cluster 0
+        count1 = 0 # count number of "faculty" docs in cluster 0
+        if labels[i] == 0 and doc[0] == "course":
+            count0 = count0 + 1
+        elif labels[i] == 0 and doc[0] == "faculty":
+            count1 = count1 + 1
+    if count0 > count1:
+        sum_count = count0
+    else:
+        sum_count = count1
+    # consider cluster 1
+    cindex = class_map["course"]
+    count0 = docnum[cindex] - count0 # number of "course" docs in cluster 1
+    count1 = docnum[1 - cindex] - count1 # number of "faculty" docs in cluster 1
+    if count0 > count1:
+        sum_count = sum_count + count0
+    else:
+        sum_count = sum_count + count1
+    
+    purity = sum_count * 1.0 / N
+    print "Purity: " + str(purity)
+    
     
 # main loop    
 def main(argv):
 #    print "File to open: " + argv[1]
     print "Usage: python ./NB-with-Gibbs.py data_file max_iters"
-    (docset, docindexset) = load_data(argv[1])
+    print "Loading data..."
+    (docset, docindexset, docnum) = load_data(argv[1])
     
     # initializations for parameters
     T = int(argv[2]) # max iterations
     N = len(docset) # number of documents
     V = len(vocabulary) # size of the vocabulary
+    print "Loading data done."
+    print "Total number of documents:" + str(N)
+    print "Number of document in category \"course\":" + str(docnum[class_map["course"]])
+    print "Number of document in category \"faculty\":" + str(docnum[class_map["faculty"]])
+    print "Size of vocabulary: " + str(V)
     
     #randomly set labels for documents
     labels = [0] * N # labes for all documents, 0 or 1
@@ -184,6 +230,8 @@ def main(argv):
     theta1 = Dirichlet(hyper_multi)
     theta = [theta0, theta1]
     
+    B = T / 3; # B is for burn-in iterations
+    class_vote = [0] * N # collect vote for every iteration
     print "Start to iterate..."
     for i in range(T):
         for j in range(N):
@@ -194,8 +242,14 @@ def main(argv):
             
             ln_Pr0 = caculate_Pr(0, class_count, N, j, docindexset, hyper_gamma, theta[0])
             ln_Pr1 = caculate_Pr(1, class_count, N, j, docindexset, hyper_gamma, theta[1])
-            ratio = exp(ln_Pr0 - ln_Pr1)
-            index = choose((0, 1), [ratio, 1]) # choose according a distribution
+            # sometimes, the probability is very overwhelming
+            if ln_Pr0 - ln_Pr1 > 13.81: # log(999999) \approx 13.815509557963773
+                index = 0
+            elif ln_Pr1 - ln_Pr0 > 13.81:
+                index = 1
+            else:
+                ratio = exp(ln_Pr0 - ln_Pr1)
+                index = choose((0, 1), [ratio, 1]) # choose according a distribution
             new_label = (0, 1)[index]
             
             labels[j] = new_label
@@ -203,10 +257,23 @@ def main(argv):
             update_word_count(docindexset[j], new_label, +1, word_count) # update word count by adding the docuemnt j
         
         update_theta(docindexset, labels, theta, hyper_multi, word_count)
-        
+        if(i >= B): # start to record data
+            for k in range(N):
+                class_vote[k] = class_vote[k] + labels[k] # record all 1s by adding every label, a bit hacking here
+        print "iteration #" + str(i) + ":"
+        print "Labels:"; print labels
+    
+    # determin labels for each document
+    for k in range(N):
+        if class_vote[k] >= T - B - class_vote[k]:
+            labels[k] = 1
+        else:
+            labels[k] = 0
+                
     print "Iteration done."
     print "Results - Labels of each document:"
     print labels
+    evaluate_result(labels, docset, N, docnum)
     
 if __name__ == "__main__":
     main(sys.argv)
